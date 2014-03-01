f409e699839b1d0979f2d7654094714144a1e346
WhiteTrefoil <whitetrefoil@gmail.com>
2014-03-02 00:48:41 +0800

    Upgrade JS libs, also included lo_dash

--- source/js/lib.js
+++ source/js/lib.js
@@ -1,4 +1,7 @@
-//= require './_lib/jquery-2.0.3.min'
-//= require './_lib/underscore-1.5.2.min'
-//= require './_lib/backbone-1.1.0.min'
-//= require './_lib/handlebars.runtime-1.0.0'
+//= require './_lib/jquery-2.1.0.min'
+//= require './_lib/underscore-1.6.0.min'
+//= require './_lib/backbone-1.1.2.min'
+//= require './_lib/handlebars.runtime-1.3.0'
+//= require './_lib/underscore_no_conflict'
+//= require './_lib/lodash-modern-2.4.1.min'
+
